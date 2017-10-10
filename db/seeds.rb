b26c51724bb4a766c2c5d6ea26ef9d1883f73939
Lara Matyushenko <larabrightside@gmail.com>
2017-10-10 17:02:51 +0000

    Added data to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,19 @@ Song.create(name: "Stylo", artist_id: gorillaz.id)
 Song.create(name: "Andromeda", artist_id: gorillaz.id)
 
 Song.create(name: "Matilda", artist_id: 1)
+
+dr = Artist.create(name: "Dune rats", user_id: 2)
+killers = Artist.create(name: "The Killers", user_id: 2)
+pond = Artist.create(name: "Pond", user_id: 2)
+
+Song.create(name: "Scott Green", artist_id: dr.id)
+Song.create(name: "Braindead", artist_id: dr.id)
+Song.create(name: "6 Pack", artist_id: dr.id)
+
+Song.create(name: "Mr. Brightside", artist_id: killers.id)
+Song.create(name: "Human", artist_id: killers.id)
+Song.create(name: "When we were young", artist_id: killers.id)
+
+Song.create(name: "Paint me silver", artist_id: pond.id)
+Song.create(name: "Zond", artist_id: pond.id)
+Song.create(name: "Holding Out for You", artist_id: pond.id)
