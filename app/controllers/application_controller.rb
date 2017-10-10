4efda31e41b4974914c02059443e3c9253fdebfe
Lara Matyushenko <larabrightside@gmail.com>
2017-10-10 15:15:36 +0000

    Done signup controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,40 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'music'
   end
 
   get "/" do
-    erb :index
+    if logged_in?
+      @user = current_user
+      erb :'/users/index'
+    else
+      erb :index
+    end
   end
 
   get '/signup' do
     erb :signup
+  end
+
+  post '/signup' do
+    @user = User.create(params[:user])
+    session[:user_id] = @user.id
+
+    redirect '/'
+  end
+
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
