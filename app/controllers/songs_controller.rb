81990ce6b2a84986be72bdc7e1e962e8db9bd8c1
Lara Matyushenko <larabrightside@gmail.com>
2017-10-11 10:12:36 +0000

    Done songs/show view page; done get '/songs/:slug/edit' controller action; song_edit form WIP

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,4 +20,19 @@ class SongsController < ApplicationController
     end
   end
 
+  get '/songs/:slug/edit' do
+    if logged_in?
+      @song = Song.find_by_slug(params[:slug])
+      if @song.user == current_user
+        erb :'/songs/edit_song'
+      else
+        flash[:message] = "You can change only songs you've added."
+        redirect to "/songs/#{@song.slug}"
+      end
+    else
+      flash[:message] = "Please login to see a song page."
+      redirect to "/login"
+    end
+  end
+
 end
