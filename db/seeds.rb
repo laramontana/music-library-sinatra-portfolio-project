d5ef04b3faceb506865c676a396648714fa15549
Lara Matyushenko <larabrightside@gmail.com>
2017-10-11 18:39:14 +0000

    Updated db/seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,23 @@ killers = Artist.create(name: "The Killers")
 pond = Artist.create(name: "Pond")
 arcade_fire = Artist.create(name: "Arcade Fire")
 
-Song.create(name: "Last Nite", artist_id: the_strokes.id, user_id: 1)
-Song.create(name: "12:51", artist_id: the_strokes.id, user_id: 2)
+alexa = User.create(username: "Alexa", password: 111111)
+bob = User.create(username: "Bob", password: 222222)
 
-Song.create(name: "DARE", artist_id: gorillaz.id, user_id: 1)
-Song.create(name: "Stylo", artist_id: gorillaz.id, user_id: 2)
+Song.create(name: "Last Nite", artist: the_strokes, user: alexa)
+Song.create(name: "12:51", artist: the_strokes, user: bob)
 
-Song.create(name: "Scott Green", artist_id: dune_rates.id, user_id: 1)
-Song.create(name: "Braindead", artist_id: dune_rates.id, user_id: 2)
+Song.create(name: "DARE", artist: gorillaz, user: alexa)
+Song.create(name: "Stylo", artist: gorillaz, user: bob)
 
-Song.create(name: "Mr. Brightside", artist_id: killers.id, user_id: 1)
-Song.create(name: "Human", artist_id: killers.id, user_id: 2)
+Song.create(name: "Scott Green", artist: dune_rates, user: alexa)
+Song.create(name: "Braindead", artist: dune_rates, user: bob)
 
-Song.create(name: "Paint me silver", artist_id: pond.id, user_id: 1)
-Song.create(name: "Holding Out for You", artist_id: pond.id, user_id: 2)
+Song.create(name: "Mr. Brightside", artist: killers, user: alexa)
+Song.create(name: "Human", artist: killers, user: bob)
 
-Song.create(name: "Reflektor", artist_id: arcade_fire.id, user_id: 1)
-Song.create(name: "Wake up", artist_id: arcade_fire.id, user_id: 2)
+Song.create(name: "Paint me silver", artist: pond, user: alexa)
+Song.create(name: "Holding Out for You", artist: pond, user: bob)
+
+Song.create(name: "Reflektor", artist: arcade_fire, user: alexa)
+Song.create(name: "Wake up", artist: arcade_fire, user: bob)
